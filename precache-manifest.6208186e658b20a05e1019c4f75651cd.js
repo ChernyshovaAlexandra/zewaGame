7c495821ec4b91643df27e3940cf536a9f5fa29c
wfc_webdev <webdev@wfc.tv>
2020-09-27 20:48:58 +0300

    Updates

---
+++ precache-manifest.6208186e658b20a05e1019c4f75651cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8f58f76983312c9c6e9d8393b9278e",
+    "url": "/zewaGame/index.html"
+  },
+  {
+    "revision": "865cff2e656acf128df7",
+    "url": "/zewaGame/static/css/main.91be667f.chunk.css"
+  },
+  {
+    "revision": "308254dde1dabe6be301",
+    "url": "/zewaGame/static/js/2.a21d5fb0.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/zewaGame/static/js/2.a21d5fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865cff2e656acf128df7",
+    "url": "/zewaGame/static/js/main.93a29f51.chunk.js"
+  },
+  {
+    "revision": "6f2d931a81d46cf094fa",
+    "url": "/zewaGame/static/js/runtime-main.d8b6e021.js"
+  },
+  {
+    "revision": "0c06b1cf423e8d47b405902fb4c7d3f9",
+    "url": "/zewaGame/static/media/2.0c06b1cf.jpg"
+  },
+  {
+    "revision": "b8a23b33b3902048dc194c8bab7a318c",
+    "url": "/zewaGame/static/media/3.b8a23b33.jpg"
+  }
+]);
