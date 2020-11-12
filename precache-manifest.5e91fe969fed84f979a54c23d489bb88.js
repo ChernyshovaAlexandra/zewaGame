da9e495e95b82c76b7f5310c3279140aca46cfac
wfc_webdev <webdev@wfc.tv>
2020-11-12 11:53:24 +0300

    Updates

---
+++ precache-manifest.5e91fe969fed84f979a54c23d489bb88.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0c54fb169885cf97031368c5366a9a",
+    "url": "/zewa/index.html"
+  },
+  {
+    "revision": "47c75052b4e4e5f53d4f",
+    "url": "/zewa/static/css/2.51afce58.chunk.css"
+  },
+  {
+    "revision": "ccc33fa536b47767cb62",
+    "url": "/zewa/static/css/main.a74c88ea.chunk.css"
+  },
+  {
+    "revision": "47c75052b4e4e5f53d4f",
+    "url": "/zewa/static/js/2.879c5727.chunk.js"
+  },
+  {
+    "revision": "7aeb96c4878d224a55adce546624b684",
+    "url": "/zewa/static/js/2.879c5727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc33fa536b47767cb62",
+    "url": "/zewa/static/js/main.aa664def.chunk.js"
+  },
+  {
+    "revision": "8e303f08b5659fa5bcf7",
+    "url": "/zewa/static/js/runtime-main.65ae4788.js"
+  },
+  {
+    "revision": "22dc932266c23b5c035d59c2e912695e",
+    "url": "/zewa/static/media/109859.22dc9322.png"
+  },
+  {
+    "revision": "3cbca9c3ad15d4d6aedc2c445557b548",
+    "url": "/zewa/static/media/AcerFoco.3cbca9c3.ttf"
+  },
+  {
+    "revision": "9dd8c37c470dcb0a87f997ce15ffb133",
+    "url": "/zewa/static/media/AcerFoco_Rg.9dd8c37c.ttf"
+  },
+  {
+    "revision": "b2e220470f08c4c8846244d3584ce8d3",
+    "url": "/zewa/static/media/Overlay.b2e22047.jpg"
+  },
+  {
+    "revision": "486a6281f588a19b254789b04bd67dd5",
+    "url": "/zewa/static/media/Rules.486a6281.pdf"
+  },
+  {
+    "revision": "abb44cfb73ef70eea1b654ec92527c6e",
+    "url": "/zewa/static/media/back.abb44cfb.png"
+  },
+  {
+    "revision": "07c831ee7c88e8cfd1d73db4bfa9e503",
+    "url": "/zewa/static/media/logoGame.07c831ee.png"
+  },
+  {
+    "revision": "a052c0ad3baa90b429557f7976d6ecf9",
+    "url": "/zewa/static/media/magnit-logo.a052c0ad.svg"
+  },
+  {
+    "revision": "ae67c8f5e0ee993a705281200348d49f",
+    "url": "/zewa/static/media/menuComp.ae67c8f5.png"
+  },
+  {
+    "revision": "8a6cef6e581cd718c98b17aa6f511add",
+    "url": "/zewa/static/media/menuComp2.8a6cef6e.png"
+  },
+  {
+    "revision": "29c2c30244b20cda680da281c6ddff4a",
+    "url": "/zewa/static/media/pattern-after-opacity.29c2c302.png"
+  },
+  {
+    "revision": "f7d6a73017dbf8bd31685ced7bcde6e8",
+    "url": "/zewa/static/media/pattern-after.f7d6a730.png"
+  },
+  {
+    "revision": "d65c900333a4ff5942a5e721403cf44a",
+    "url": "/zewa/static/media/pattern.d65c9003.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/zewa/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/zewa/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/zewa/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/zewa/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "37e618f6a5635be860ed9e3bf95b609c",
+    "url": "/zewa/static/media/zewa.37e618f6.png"
+  }
+]);
