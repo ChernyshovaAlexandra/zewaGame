afcc6b1e51d24ba3be60c83c52133c751b526038
wfc_webdev <webdev@wfc.tv>
2021-03-22 22:01:33 +0300

    f

--- src/components/QuestWinModal/WinBlocks.jsx
+++ src/components/QuestWinModal/WinBlocks.jsx
@@ -43,7 +43,7 @@ const Back = ({ amount, logo, company, clickToShowRepost }) => {
 
 
 export const WinBlocks = ({ logo, flipped, toggleCard, canClick, selected, amount, showRepost, clickToShowRepost }) => {
-    console.log(logo, flipped, canClick, selected, amount, showRepost)
+
 
     if (selected) {
         let item = cards[flipped]
